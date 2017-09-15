16ec94a5b32f568fc0533f7b814859ce22acd22c
Laurensius Dede Suhardiman <burglar.tea@gmail.com>
2017-09-15 12:52:12 +0700

    ESP8266 Siap terima instruksi dari Serial

--- fix.ino
+++ fix.ino
@@ -7,28 +7,38 @@ const char* password = "8cadb11f56";
 void setup () {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
+  Serial.println("Setting ESP8266");
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Menghubungkang ke Access Point..");
     delay(1000);
-    Serial.print("Connecting..");
   }
 }
  
 void loop() {
+   while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("ESP8266 tidak terhubung ke Access Point");
+    Serial.println("Menghubungkang ke Access Point..");
+    delay(1000);
+  }
   if(Serial.available()){
-    String server = "http://192.168.0.101/lampu/index.php";
     String incoming = Serial.readString();
-    String kirim = server + incoming;
     if (WiFi.status() == WL_CONNECTED) { 
       HTTPClient http;  
-      http.begin(kirim);  
+      http.begin(incoming);  
       int httpCode = http.GET();                                                       
       if (httpCode > 0) { 
         String payload = http.getString();  
+        Serial.println("HTTP Respon dari server : ");
+        Serial.println(httpCode);  
+        Serial.println("HTTP Respon payload : ");
         Serial.println(payload);  
       }
       http.end();   
+    }else{
+      Serial.println("ESP8266 tidak terhubung ke Access Point");  
     }
   }else{
-    Serial.println("Nothing");
-  }delay(1000);
+    Serial.println("Menunggu paket data untuk dikirim ke server . . . .");
+  }
+  delay(1000);
 }
